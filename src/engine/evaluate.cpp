4d8e81c15f45917e5bc9bdd64d57998de0fd6e3e
Nate Lamarche <natealamarche@gmail.com>
2026-09-01 00:20:24 -0400

    feat: removed material eval

--- src/engine/evaluate.cpp
+++ src/engine/evaluate.cpp
@@ -4,42 +4,6 @@
 #include <cmath>
 
 namespace chess::engine {
-namespace {
-
-int piece_value(Piece piece){
-    switch (piece) {
-        case Piece::Pawn: return 100;
-        case Piece::Knight: return 300;
-        case Piece::Bishop: return 300;
-        case Piece::Rook: return 500;
-        case Piece::Queen: return 900;
-        case Piece::King: return 0;
-        case Piece::None: return 0;
-    }
-
-    return 0;
-
-}
-
-int material_eval(const Board& board){
-    int score = 0;
-
-    for (Square sq = 0; sq < 64; sq++){
-        const PieceOnSquare& target = board.at(sq);
-        
-        if (target.colour == Colour::White){
-            score += piece_value(target.piece);
-        } else {
-            score -= piece_value(target.piece);
-        }
-    }
-
-    return score;
-
-}
-
-
-}
 
 int evaluate(const Board& board, const NnueModel& model){
 
